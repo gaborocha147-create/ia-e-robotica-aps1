1a9e8728c5b20ce6f04bd86b69ae150103466981
humberto-filho <humbertofilhocastr@gmail.com>
2026-08-27 15:53:02 -0300

    feat: gera_grrid + buscas (base)

---
+++ buscas.py
@@ -0,0 +1,54 @@
+from aigyminsper.search.search_algorithms import BuscaLargura
+from aigyminsper.search.graph import State
+
+class AgentSpecification(State):
+
+    def __init__(self, op):
+        # voce sempre deve usar esta chamada para inicializar a superclasse
+        super().__init__(op)
+        #TODO
+    
+    def successors(self):
+        successors = []
+        #TODO
+        return successors
+    
+    def is_goal(self):
+        pass
+    
+    def description(self):
+        return "Descrição do problema"
+    
+    def cost(self):
+        return 1
+    
+    def env(self):
+        #
+        # IMPORTANTE: este método não deve apenas retornar uma descrição do environment, mas 
+        # deve também retornar um valor que descreva aquele nodo em específico. Pois 
+        # esta representação é utilizada para verificar se um nodo deve ou ser adicionado 
+        # na lista de abertos.
+        #
+        # Exemplos de especificações adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)+"#"+str(self.cost)
+        # - para o problema das cidades: return self.city+"#"+str(self.cost())
+        #
+        # Exemplos de especificações NÃO adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)
+        # - para o problema das cidades: return self.city
+        #
+        None
+
+def main():
+    print('Busca em largura')
+    state = AgentSpecification('')
+    algorithm = BuscaLargura()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
+
+if __name__ == '__main__':
+    main()
